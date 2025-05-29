dcc2d1f77caf479cc50031dbefaf5c97bf113678
Naveen pandian <npandian515@gmail.com>
2025-05-30 01:18:51 +0530

    Refactor code structure for Decision Tree and Random Forest model training and predictions in DecNRan.py

--- DecNRan.py
+++ DecNRan.py
@@ -14,3 +14,17 @@ X = iris.data
 y = iris.target
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+dt_model = DecisionTreeClassifier()
+
+dt_model.fit(x_train, y_train)
+
+dt_pred = dt_model.predict(x_test)
+
+
+rf_model = RandomForestClassifier()
+
+rf_model.fit(x_train, y_train)
+
+rf_pred = rf_model.predict(x_test)
