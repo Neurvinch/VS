3ec98e005e03c3db4a335c505527c231f9be3caf
Naveen pandian <npandian515@gmail.com>
2025-05-30 01:14:35 +0530

    Add implementation of multi-linear regression model with predictions and evaluation metrics

--- MultLinReg.py
+++ MultLinReg.py
@@ -4,3 +4,20 @@ from sklearn.linear_model import LinearRegression
 
 from sklearn.metrics import mean_absolute_error, r2_score
 
+
+x = np.array([[1,1], [2,2], [3,3], [4,4], [5,5]])
+
+y = np.array([1, 2, 3, 4, 5])
+
+model = LinearRegression()
+
+model.fit(x, y)
+
+y_pred= model.predict(x)
+
+model.intercept_
+
+model.coef_
+
+mean_absolute_error(y, y_pred)
+r2_score(y, y_pred)
