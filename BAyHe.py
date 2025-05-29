321692f25f0ee11f8c444d637b9a94c072db32b8
Naveen pandian <npandian515@gmail.com>
2025-05-30 00:47:57 +0530

    Fix variable name typo and improve data preprocessing steps in Bayesian model

--- BAyHe.py
+++ BAyHe.py
@@ -12,3 +12,22 @@ url  =" ";
 columns = ['A', 'B', 'C', 'D', 'E']
 
 df =pd.read_csv(url, names = columns, na_values='?')
+
+df = df.dropna()
+
+df["A"] = pd.cut(df['A'], bin = 3, labels = ["a", "b", "c"])
+
+
+model = BayesianModel([
+    ("A",'target'),
+    ("B",'target'),
+]);
+
+
+model.fit(df, estimator = MaximumLikelihoodEstimator)
+
+inference = VariavleElimination(model)
+
+query = inference.query(variables=['targer'], evidence={'age': 'a', 'cp':3})
+
+print(query)
