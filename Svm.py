b7a665d7a7437993b84f00a8c62d8cb291241459
Naveen pandian <npandian515@gmail.com>
2025-05-30 01:22:11 +0530

    Add initial implementation of SVM model with necessary imports in Svm.py

---
+++ Svm.py
@@ -0,0 +1,7 @@
+from sklearn import datasets
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.svm import SVC, SVR
+
+from sklearn.metrics import accuracy_score, mean_squared_error , classification_report
