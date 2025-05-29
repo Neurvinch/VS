245d5b67e70814fbe75f67ae6481cc3efb2e597d
Naveen pandian <npandian515@gmail.com>
2025-05-30 00:18:06 +0530

    Add accuracy score and classification report print statements

--- BeNA.py
+++ BeNA.py
@@ -22,3 +22,5 @@ bnb.fit(X_train, y_train)
 
 y_pred = bnb.predict(X_test);
 
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
