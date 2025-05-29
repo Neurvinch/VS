d92379243633cc0f984a697dca491967dffc9d20
Naveen pandian <npandian515@gmail.com>
2025-05-30 01:06:11 +0530

    Fix typo in Linear Regression model: change 'conef_' to 'coef_'

--- LinReg.py
+++ LinReg.py
@@ -5,3 +5,30 @@ from sklearn.linear_model import LinearRegression
 
 from sklearn.metrics import mean_squared_error, r2_score
 
+x = np.array([[1], [2], [3], [4], [5]])
+
+y = np.array([1, 2, 3, 4, 5])
+
+
+model = LinearRegression()
+
+model.fit(x, y)
+
+y_pred = model.predict(x)
+
+model.conef_[0]
+model.intercept_
+mean_squared_error(y, y_pred)
+r2_score(y,y_pred)
+
+
+plt.scatter(x, y , color='blue', label='Data Points')
+plt.plot(x, y_pred, color= 'red', label='Regression Line')
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Linear Regression Example')
+
+plt.legend()
+
+plt.show()
