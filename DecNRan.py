8ecaa52c2ece5250c8698db3acae8aa746932cbf
Naveen pandian <npandian515@gmail.com>
2025-05-30 01:16:18 +0530

    Add initial imports for DecNRan.py

---
+++ DecNRan.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import load_iris
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+from sklearn.model_selection import train_test_split
+
+from 
