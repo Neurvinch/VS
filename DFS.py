be5908a7680ea36c8869453d90b8a40b93eb4f6d
Naveen pandian <npandian515@gmail.com>
2025-05-30 08:40:36 +0530

    Add initial DFS implementation in DFS.py with graph representation

---
+++ DFS.py
@@ -0,0 +1,12 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+
+ def dfs(graph , start , visited = None , result = None ):
+    
