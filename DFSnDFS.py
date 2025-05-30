f8ee58269ae43593ef984559a77718b07c4ba918
Naveen pandian <npandian515@gmail.com>
2025-05-30 11:03:30 +0530

    Add BFS and initial DFS function implementations in DFSnDFS.py

---
+++ DFSnDFS.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+def bfs(graph , start):
+    visited = set()
+    queue = deque([start])
+
+
+    while queue:
+        node = queue.popleft()
+
+        if node not in visited:
+            visited.add(node)
+            queue.extend(neighbour for neighbour in graph[node] if neighbour not in visited)
+
+
+
+def dfs ()
+
+
+
