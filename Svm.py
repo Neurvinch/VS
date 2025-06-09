3a21de2a3840fafde55d07890b582a7e315280c6
Naveen Pandian <149451318+Neurvinch@users.noreply.github.com>
2025-06-09 19:09:28 +0530

    Update Svm.py

--- Svm.py
+++ Svm.py
@@ -12,7 +12,7 @@ iris = datasets.load_iris()
 X_cls = iris.data
 
 y_cls = iris.target
-
+ 
 X_train_cls, X_test_cls, y_train_cls, y_test_cls = train_test_split(X_cls, y_cls, test_size=0.3, random_state=42)
 
 svm_classifier = SVC(kernel=    'linear')
